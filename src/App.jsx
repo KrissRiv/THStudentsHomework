e933a0cae73c5202863a9034e10209879c168a66
KrissRiv <cristianriv@gmail.com>
2023-11-16 14:45:31 -0500

    SUCCESS: Test for render list after search action

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
+import { useState } from 'react';
 import './App.css'
 
 function App () {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [listItems, setListItms] = useState([]);
+
+  const onSearch = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handlerSearch = () => {
+    setListItms([searchTerm]);
+  }
+
   return (
     <>
       TH Students Homework
       <hr />
-      <input autoFocus data-testid="search" />
-      <button data-testid="go">Go</button>
+      <input onChange={(e) => onSearch(e)} autoFocus data-testid="search" />
+      <button onClick={handlerSearch} data-testid="go">Go</button>
+      {listItems.length > 0 && (
+        <ul data-testid="courses-collection">
+          <li data-testid="course"></li>
+        </ul>
+      )}
     </>
   )
 }
