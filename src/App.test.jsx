78045e987b824789c1945ad5e3b48babb7fbdcc8
KrissRiv <cristianriv@gmail.com>
2023-11-16 17:20:17 -0500

    FAIL: Test for call API service

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,7 +1,33 @@
-import { fireEvent, render, screen } from '@testing-library/react'
-import { describe } from 'vitest'
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { describe, expect } from "vitest";
+import { HttpResponse, http } from "msw";
+import { setupServer } from "msw/node";
 
-import App from './App'
+import App from "./App";
+
+const mockData = [
+ {
+   id: 1,
+   name: "First class",
+   parent_id: 1,
+ },
+ {
+   id: 2,
+   name: "Second class",
+   parent_id: 1,
+ },
+ {
+   id: 3,
+   name: "Other class",
+   parent_id: 2,
+ },
+];
+
+const server = setupServer(
+ http.get("/service-courses", (request, params, cookies) => {
+   return HttpResponse.json(mockData);
+ })
+);
 
 describe("App", () => {
   it('Contains heading', () => {
@@ -52,4 +78,18 @@ describe("App", () => {
     const button = screen.getByTestId("go")
     expect(button).toBeInTheDocument()
   })
+  it("Call API service for courses", async () => {
+    server.listen();
+
+    render(<App url={"/service-courses"} />);
+    const button = screen.getByTestId("go");
+    fireEvent.click(button);
+
+    waitFor(() => {
+      expect(screen.getAllByTestId("course").length).toBe(mockData.length);
+    });
+    
+    server.resetHandlers()
+    server.close();
+  });
 });
