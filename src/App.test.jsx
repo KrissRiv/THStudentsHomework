7dde80cba7588533271f64d211ef0ee7af2e4482
KrissRiv <cristianriv@gmail.com>
2023-11-17 10:28:16 -0500

    FAIL: Test for loading message

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,98 +1,46 @@
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
-import { describe, expect } from "vitest";
-import { HttpResponse, http } from "msw";
-import { setupServer } from "msw/node";
+import {
+  cleanup,
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 
 import App from "./App";
-
-const mockData = [
- {
-   id: 1,
-   name: "First class",
-   parent_id: 1,
- },
- {
-   id: 2,
-   name: "Second class",
-   parent_id: 1,
- },
- {
-   id: 3,
-   name: "Other class",
-   parent_id: 2,
- },
-];
-
-const server = setupServer(
- http.get("/service-courses", (request, params, cookies) => {
-   return HttpResponse.json(mockData);
- })
-);
+import { afterEach, beforeEach, describe, expect, it } from "vitest";
 
 describe("App", () => {
+  afterEach(cleanup);
+  beforeEach(() => render(<App />));
+  /**
+   * Test for UI Interface
+   */
   it("Contains heading", () => {
-    render(<App />);
     const heading = screen.getByText("TH Students Homework");
     expect(heading).toBeInTheDocument();
   });
   it("Contain input search", () => {
-    render(<App />);
     const input = screen.getByTestId("search");
     expect(input).toBeInTheDocument();
   });
   it("Contain input have focus", () => {
-    render(<App />);
     const input = screen.getByTestId("search");
     expect(input.matches(":focus")).toBe(true);
   });
-  it("Contain list collection after search action", () => {
-    const mockValue = "search term";
- 
-    render(<App url={"/service-courses"} />);
-    const button = screen.getByTestId("go");
-    const input = screen.getByTestId("search");
- 
-    fireEvent.change(input, { target: { value: mockValue } });
-    fireEvent.click(button);
- 
-    waitFor(() => {
-      const courses = screen.getByTestId("courses-collection");
-      expect(courses).toBeInTheDocument();
-    });
-  });
-  it("Contain list collection after search action when press enter key", () => {
-    const mockValue = "search term";
- 
-    render(<App url={"/service-courses"} />);
-    const button = screen.getByTestId("go");
-    const input = screen.getByTestId("search");
- 
-    fireEvent.change(input, { target: { value: mockValue } });
-    fireEvent.keyUp(input, { key: "Enter", keyCode: 13 });
-    
-    waitFor(() => {
-      const courses = screen.getByTestId("courses-collection");
-      expect(courses).toBeInTheDocument();
-    });
-  });
   it("Contain button", () => {
-    render(<App />);
     const button = screen.getByTestId("go");
     expect(button).toBeInTheDocument();
   });
- 
-  it("Call API service for courses", async () => {
-    server.listen();
 
-    render(<App url={"/service-courses"} />);
-    const button = screen.getByTestId("go");
+  /**
+   * Test for API service
+   */
+  it("Contain Loading message after search action", async () => {
+    const button = screen.getByRole("button", { name: /Go/i });
+
     fireEvent.click(button);
- 
-    waitFor(() => {
-      expect(screen.getAllByTestId("course").length).toBe(mockData.length);
-    });
-   
-    server.resetHandlers()
-    server.close();
+
+    expect(screen.getByText("Loading...")).toBeInTheDocument();
   });
- }); 
+});
