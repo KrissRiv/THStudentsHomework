44f1a237bf97f13c10b62bace6eaed7f414f9923
KrissRiv <cristianriv@gmail.com>
2023-11-17 11:14:04 -0500

    SUCCESS: Test for cases: Parent 0 and childs with: - and subchilds with: --

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -36,11 +36,45 @@ describe("App", () => {
   /**
    * Test for API service
    */
-  it("Contain Loading message after search action", async () => {
+  it("Contain Loading message after search action", () => {
     const button = screen.getByRole("button", { name: /Go/i });
 
     fireEvent.click(button);
 
     expect(screen.getByText("Loading...")).toBeInTheDocument();
   });
+  it("Contain list collection after search action: First case parent 0", async () => {
+    const button = screen.getByRole("button", { name: /Go/i });
+
+    fireEvent.click(button);
+    await waitForElementToBeRemoved(() => screen.queryByText("Loading..."))
+
+    expect(screen.getByText("Module 15: Acids and Bases")).toBeInTheDocument();
+    expect(screen.getByText("Module 5: Thermochemistry")).toBeInTheDocument();
+    expect(screen.getByText("Module 18: Electrochemistry - Practical Applications of Gibbs Free Energy")).toBeInTheDocument();
+
+  });
+  it("Contain child list of the parent: Second case -", async () => {
+    const button = screen.getByRole("button", { name: /Go/i });
+
+    fireEvent.click(button);
+    await waitForElementToBeRemoved(() => screen.queryByText("Loading..."))
+
+    expect(screen.getAllByText(/-/i).length).toBeGreaterThan(0);
+    expect(screen.getByText(/Homework Assignment: Acids and Bases/i)).toBeInTheDocument();
+    expect(screen.getByText(/Test Bank: Acids and Bases/i)).toBeInTheDocument();
+
+  });
+  it("Contain child list of the parent: Third case --", async () => {
+    const button = screen.getByRole("button", { name: /Go/i });
+
+    fireEvent.click(button);
+    await waitForElementToBeRemoved(() => screen.queryByText("Loading..."))
+
+    expect(screen.getAllByText(/--/i).length).toBeGreaterThan(0);
+    expect(screen.getByText(/Question 13.2/i)).toBeInTheDocument();
+    expect(screen.getByText(/Question 13.4/i)).toBeInTheDocument();
+
+  });
+ 
 });
