164f7a42987e11688e3795914a66213329cc5710
KrissRiv <cristianriv@gmail.com>
2023-11-20 21:06:05 -0500

    REFACTOR: add coverage script on vitest ui

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   projectId: "tv7ghs",
   e2e: {
-    baseUrl: "http://localhost:3000",
+    baseUrl: "https://krissriv.github.io/THStudentsHomework/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
